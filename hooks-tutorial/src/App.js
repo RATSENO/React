60560a4c94a92e89049b0d75fe815788f55cf948
ratseno <onestar1014@gmail.com>
2020-03-26 22:10:12 +0900

    hooks - useState

---
+++ hooks-tutorial/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+//import Counter from "./Counter";
+import Info from "./Info";
+
+function App() {
+  return (
+    <div>
+      <Info></Info>
+    </div>
+  );
+}
+
+export default App;
