57e7ee0f62eb45a309a6e807832d6fd26a79fa02
ratseno <onestar1014@gmail.com>
2020-03-30 20:44:42 +0900

    hook - useEffect

--- hooks-tutorial/src/App.js
+++ hooks-tutorial/src/App.js
@@ -1,13 +1,21 @@
-import React from "react";
-import logo from "./logo.svg";
+import React, { useState } from "react";
 import "./App.css";
 //import Counter from "./Counter";
 import Info from "./Info";
 
 function App() {
+  const [visible, setVisible] = useState(false);
   return (
     <div>
-      <Info></Info>
+      <button
+        onClick={() => {
+          setVisible(!visible);
+        }}
+      >
+        {visible ? "숨기기" : "보이기"}
+      </button>
+      <hr />
+      {visible && <Info />}
     </div>
   );
 }
