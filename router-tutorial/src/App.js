5e1d11f9f11f9d01e4d8ad4790ae7710ce889cf7
ratseno <onestar1014@gmail.com>
2020-04-07 22:06:17 +0900

    URL파라미터, 쿼리

--- router-tutorial/src/App.js
+++ router-tutorial/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Route, Link } from "react-router-dom";
 import Home from "./Home";
 import About from "./About";
+import Profile from "./Profile";
 
 const App = () => {
   return (
@@ -13,9 +14,16 @@ const App = () => {
         <li>
           <Link to="/about">소개</Link>
         </li>
+        <li>
+          <Link to="/profile/velopert">velopert 프로필</Link>
+        </li>
+        <li>
+          <Link to="/profile/gildong">gildong 프로필</Link>
+        </li>
       </ul>
       <Route path="/" component={Home} exact={true}></Route>
       <Route path="/about" component={About}></Route>
+      <Route path="/profile/:username" component={Profile}></Route>
     </div>
   );
 };
