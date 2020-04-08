6981ce25e838c34152179a00050421677645327d
ratseno <onestar1014@gmail.com>
2020-04-08 21:23:32 +0900

    async

--- news-viewer/src/App.js
+++ news-viewer/src/App.js
@@ -1,15 +1,25 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-
+//724f88c9764345ab83fcca9f31b1e4b7
 const App = () => {
   const [data, setData] = useState(null);
-  const onClick = () => {
-    axios
-      .get('https://jsonplaceholder.typicode.com/todos/1')
-      .then((response) => {
-        setData(response.data);
-      });
+
+  const onClick = async () => {
+    try {
+      const response = await axios.get(
+        'https://jsonplaceholder.typicode.com/todos/1',
+      );
+      setData(response.data);
+    } catch (error) {}
   };
+  // const onClick = () => {
+  //   axios
+  //     .get('https://jsonplaceholder.typicode.com/todos/1')
+  //     .then((response) => {
+  //       setData(response.data);
+  //     });
+  // };
+
   return (
     <div>
       <div>
